06688cfa273cb6c234ad4ba6d35feb098bb921ef
Aizy Vera <aizyvc@gmail.com>
2026-06-18 11:19:52 -0500

    Hola

---
+++ src/Modelo/Usuario.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.time.LocalDate;
+
+public class Usuario {
+
+    private int idUsuario;
+    private String nombre;
+    private String correo;
+    private String password;
+    private String carrera;
+    private LocalDate fechaRegistro;
+
+    public Usuario(String nombre, String correo, String password, String carrera, LocalDate fechaRegistro) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.password = password;
+        this.carrera = carrera;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public Usuario(int idUsuario, String nombre, String correo, String password, String carrera, LocalDate fechaRegistro) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.password = password;
+        this.carrera = carrera;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+}
