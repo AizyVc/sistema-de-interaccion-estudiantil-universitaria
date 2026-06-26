04164df5c5401efab49d64a61c73a9c59747ba60
Aizy Vera <aizyvc@gmail.com>
2026-06-26 11:52:16 -0500

    Metodo registrarUsuario

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -16,18 +16,21 @@ public class Conexion {
     private static final String PASSWORD = "aHRfNgtZIBeHM4k5PEFaz3ZmsWdrNjEY";
     private static final String URL = "jdbc:postgresql://" + HOST + ":" + PORT + "/" + DB_NAME + "?sslmode=require";
 
+    private Conexion (){
+        
+    }
     public static Connection obtenerConexion() {
         Connection conexion = null;
         try {
             Class.forName("org.postgresql.Driver");
             conexion = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Conectado exitosamente a la nube de Render");
+            System.out.println("Conectado exitosamente");
         } catch (ClassNotFoundException e) {
             System.out.println("ERROR! No se encontró el driver JDBC de PostgreSQL en Libraries.");
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         } catch (SQLException e) {
             System.out.println("ERROR! No se pudo conectar a Render. Revisa tu internet");
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
         return conexion;
     }
--- src/Modelo/DAO/UsuarioDAO.java
+++ src/Modelo/DAO/UsuarioDAO.java
@@ -4,10 +4,45 @@
  */
 package Modelo.DAO;
 
+import Conexion.Conexion;
+import Modelo.DTO.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author User
  */
 public class UsuarioDAO {
-    
+
+    public Connection conexion;
+
+    public UsuarioDAO() {
+        conexion = Conexion.obtenerConexion();
+    }
+
+    public boolean registrarUsuario(Usuario usuario) {
+        String sql
+                = "INSERT INTO usuarios(nombre, correo, contraseña, carrera)"
+                + "VALUES (?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setString(1, usuario.getNombre());
+            ps.setString(2, usuario.getCorreo());
+            ps.setString(3, usuario.getContrasena());
+            ps.setString(4, usuario.getCarrera());
+            int filas = ps.executeUpdate();
+            if (filas > 0) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
 }
