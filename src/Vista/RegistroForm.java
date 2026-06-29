10cc6e2e8f89c4445d334be576b9f68942761fe1
Nury Andrade <nugeanhe@gmail.com>
2026-06-28 19:45:38 -0500

    avances esteticos login y registro

--- src/Vista/RegistroForm.java
+++ src/Vista/RegistroForm.java
@@ -20,6 +20,7 @@ public class RegistroForm extends javax.swing.JFrame {
      */
     public RegistroForm() {
         initComponents();
+        this.setLocationRelativeTo(null);
         usuarioController = new UsuarioController();
     }
 
@@ -47,6 +48,10 @@ public class RegistroForm extends javax.swing.JFrame {
         btnRegistrarse = new javax.swing.JButton();
         jLabel7 = new javax.swing.JLabel();
         lblIniciarSesion = new javax.swing.JLabel();
+        jSeparator2 = new javax.swing.JSeparator();
+        jSeparator3 = new javax.swing.JSeparator();
+        jSeparator4 = new javax.swing.JSeparator();
+        jSeparator5 = new javax.swing.JSeparator();
         jLabel8 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -59,52 +64,63 @@ public class RegistroForm extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Century Gothic", 1, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(0, 32, 94));
-        jLabel1.setText("Registro");
+        jLabel1.setText("Crear cuenta");
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 10, -1, -1));
 
         jLabel2.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(0, 32, 94));
         jLabel2.setText("Nombre");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 60, -1, -1));
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 60, -1, -1));
 
+        txtNombre.setBackground(new java.awt.Color(250, 251, 252));
         txtNombre.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
-        jPanel1.add(txtNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 80, 256, -1));
+        txtNombre.setBorder(null);
+        jPanel1.add(txtNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 80, 256, -1));
 
         jLabel3.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(0, 32, 94));
-        jLabel3.setText("Correo");
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 120, -1, -1));
+        jLabel3.setText("Correo Electrónico");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 120, -1, -1));
 
+        txtCorreo.setBackground(new java.awt.Color(250, 251, 252));
         txtCorreo.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
-        jPanel1.add(txtCorreo, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 140, 256, -1));
+        txtCorreo.setBorder(null);
+        jPanel1.add(txtCorreo, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 140, 256, -1));
 
         jLabel4.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(0, 32, 94));
         jLabel4.setText("Carrera");
-        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 180, -1, -1));
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 180, -1, -1));
 
-        cmbCarrera.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Escoga una opcion", "Software", "Tecnologia de la Informacion", "IA ", "Ingenieria Civil" }));
-        jPanel1.add(cmbCarrera, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 200, 256, -1));
+        cmbCarrera.setForeground(new java.awt.Color(204, 204, 204));
+        cmbCarrera.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccione su carrera...", "Software", "Tecnologia de la Informacion", "IA", "Ingenieria Civil" }));
+        jPanel1.add(cmbCarrera, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 200, 256, -1));
 
         jLabel5.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jLabel5.setForeground(new java.awt.Color(0, 32, 94));
         jLabel5.setText("Contraseña");
-        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 240, -1, -1));
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 240, -1, -1));
 
+        txtContrasena.setBackground(new java.awt.Color(250, 251, 252));
         txtContrasena.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
-        jPanel1.add(txtContrasena, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 260, 256, -1));
+        txtContrasena.setBorder(null);
+        jPanel1.add(txtContrasena, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 260, 256, -1));
 
         jLabel6.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(0, 32, 94));
         jLabel6.setText("Confirmar Contraseña");
-        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 300, -1, -1));
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 300, -1, -1));
 
+        txtConfirmarContrasena.setBackground(new java.awt.Color(250, 251, 252));
         txtConfirmarContrasena.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
-        jPanel1.add(txtConfirmarContrasena, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 330, 256, -1));
+        txtConfirmarContrasena.setBorder(null);
+        jPanel1.add(txtConfirmarContrasena, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 330, 256, -1));
 
-        btnRegistrarse.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
-        btnRegistrarse.setForeground(new java.awt.Color(0, 32, 94));
+        btnRegistrarse.setBackground(new java.awt.Color(0, 32, 94));
+        btnRegistrarse.setFont(new java.awt.Font("Century Gothic", 0, 16)); // NOI18N
+        btnRegistrarse.setForeground(new java.awt.Color(255, 255, 255));
         btnRegistrarse.setText("Registrarse");
+        btnRegistrarse.setBorder(null);
         btnRegistrarse.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 btnRegistrarseMouseClicked(evt);
@@ -115,11 +131,11 @@ public class RegistroForm extends javax.swing.JFrame {
                 btnRegistrarseActionPerformed(evt);
             }
         });
-        jPanel1.add(btnRegistrarse, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 370, -1, -1));
+        jPanel1.add(btnRegistrarse, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 370, 110, 30));
 
         jLabel7.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         jLabel7.setText("¿Ya tienes una cuenta?");
-        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 410, -1, -1));
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 410, -1, -1));
 
         lblIniciarSesion.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         lblIniciarSesion.setForeground(new java.awt.Color(0, 0, 255));
@@ -130,14 +146,31 @@ public class RegistroForm extends javax.swing.JFrame {
                 lblIniciarSesionMouseClicked(evt);
             }
         });
-        jPanel1.add(lblIniciarSesion, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 410, -1, -1));
+        jPanel1.add(lblIniciarSesion, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 410, -1, -1));
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 100, 390, 440));
+        jSeparator2.setForeground(new java.awt.Color(0, 32, 94));
+        jSeparator2.setMaximumSize(new java.awt.Dimension(32767, 35));
+        jPanel1.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 350, 260, 10));
 
-        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/fondo_ug.png"))); // NOI18N
-        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+        jSeparator3.setForeground(new java.awt.Color(0, 32, 94));
+        jSeparator3.setMaximumSize(new java.awt.Dimension(32767, 35));
+        jPanel1.add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 100, 260, 10));
+
+        jSeparator4.setForeground(new java.awt.Color(0, 32, 94));
+        jSeparator4.setMaximumSize(new java.awt.Dimension(32767, 35));
+        jPanel1.add(jSeparator4, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 160, 260, 10));
+
+        jSeparator5.setForeground(new java.awt.Color(0, 32, 94));
+        jSeparator5.setMaximumSize(new java.awt.Dimension(32767, 35));
+        jPanel1.add(jSeparator5, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 280, 260, 10));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 100, 450, 480));
+
+        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/fondo.png"))); // NOI18N
+        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1020, 680));
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnRegistrarseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarseActionPerformed
@@ -253,6 +286,10 @@ public class RegistroForm extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JSeparator jSeparator3;
+    private javax.swing.JSeparator jSeparator4;
+    private javax.swing.JSeparator jSeparator5;
     private javax.swing.JLabel lblIniciarSesion;
     private javax.swing.JPasswordField txtConfirmarContrasena;
     private javax.swing.JPasswordField txtContrasena;
